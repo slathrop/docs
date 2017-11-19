1ee0532dcad78917d90697e41dc41e40e89ded62
John Leider <john.j.leider@gmail.com>
2017-11-19 18:20:35 -0500

    fixed unhandled exception

--- pages/ExampleView.vue
+++ pages/ExampleView.vue
@@ -16,13 +16,14 @@
     },
 
     beforeRouteEnter (to, from, next) {
-      console.log(to.params.example)
       return import(
         /* webpackChunkName: "examples" */
         /* webpackMode: "lazy-once" */
         `../examples/${to.params.example}.vue`
       ).then(comp => {
         next(vm => vm.component = comp)
+      }).catch(e => {
+        next('/')
       })
     }
   }
